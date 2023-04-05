b601995d7fc21ac744c101f618aed7e6b0c484c5
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-04-05 14:58:56 +0500

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,29 @@
+                                // Loops and Data ControlFlow
+
+// console.log("Hello")
+// console.log("Hello")
+// console.log("Hello")
+// console.log("Hello")
+// console.log("Hello")
+                                        // For Loop
+
+// for (let index = 1; index <= 5; index++) {
+//     console.log(index,"Hello");
+    
+// }
+                                        // While Loop
+                                        
+// let i = 1
+// while (i<=5) {
+//     console.log(i,"Hello") 
+//     i++   
+// }
+
+                                        // Do While Loop
+
+// let j = 1
+// do {
+//     console.log(j,"Hello")  
+//     j++  
+// } while (j<=5);
+
